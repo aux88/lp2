1056df97d69e21e8999ba2d4c2e994c54351b0c9
yoshimi.satou <youhes@gmail.com>
2025-07-23 11:42:12 +0900

    SP版 Swiper実装

---
+++ assets/script/main.js
@@ -0,0 +1,28 @@
+
+const mySwiper = new Swiper('.swiper', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 1,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 3, // 768px以上の場合の表示数
+      spaceBetween: 40  // 必要に応じてスライド間の余白を変更可能
+    }
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    type: "bullets",
+  },
+  /*
+  autoplay: { //自動再生の設定
+    delay: 2500,
+    disableOnInteraction: false,
+  },*/
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
